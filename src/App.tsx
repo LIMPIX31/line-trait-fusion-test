2ef722c7405b29956e0c999897fb58c05d0a1a17
LIMPIX31 <limpix31@gmail.com>
2022-10-31 19:48:01 +0100

    feat: main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,23 @@
 import { FC } from 'react'
+import { useCharacter } from 'hooks/useCharacter'
+import { CharacterPreview } from 'components/molecules/CharacterPreview'
+import styled from 'styled-components'
+import { CharacterEditor } from 'components/organisms/CharacterEditor/CharacterEditor'
+
+const Center = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+  gap: 1rem;
+`
 
 export const App: FC = () => {
-  return <div>123</div>
+  const character = useCharacter()
+
+  return <Center>
+    <CharacterPreview character={character} />
+    <CharacterEditor character={character} />
+  </Center>
 }
