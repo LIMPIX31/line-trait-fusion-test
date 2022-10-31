621dac710f6c3bfca97a28394334b1e36d8a5bb1
LIMPIX31 <limpix31@gmail.com>
2022-10-31 19:47:42 +0100

    feat: doesn't show icon if not provided

--- src/components/molecules/Attribute/Attribute.tsx
+++ src/components/molecules/Attribute/Attribute.tsx
@@ -7,7 +7,7 @@ import { Glyph } from 'components/atoms/Glyph'
 export const Attribute: FC<IE['div'] & AttributeProps> = ({ min, max, value, color, children, ...props }) => {
   return (
     <Wrapper color={color} {...(props as object)}>
-      <Glyph>{children}</Glyph>
+      {children && <Glyph>{children}</Glyph>}
       <Bar min={min} max={max} value={value} color={color} />
     </Wrapper>
   )
